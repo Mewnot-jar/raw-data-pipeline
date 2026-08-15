3957c5e8a84c9397e5645ce7cdd2235a12e3c8ec
Mewnot-jar <martinardiles.a@gmail.com>
2026-08-15 19:52:11 -0400

    feat: add Drive traversal for company folders and files

--- src/drive_client.py
+++ src/drive_client.py
@@ -1,11 +1,12 @@
 import os
-from config.settings import CREDENTIALS_PATH
+from config.settings import CREDENTIALS_PATH, DRIVE_ROOT_FOLDER_ID
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
+#Construye el servicio drive con las credenciales CREDENTIALS_PATH
 def build_drive_service():
     
     credentials_path = CREDENTIALS_PATH
@@ -19,6 +20,7 @@ def build_drive_service():
 
     return service
 
+#Lista las carpetas de las compañias en el drive
 def list_company_folders(service, root_folder_id):
     query = f"'{root_folder_id}' in parents and mimeType = 'application/vnd.google-apps.folder'"
 
@@ -29,10 +31,32 @@ def list_company_folders(service, root_folder_id):
 
     return result["files"]
 
+#Lista los archivos dentro de la compañia con company_folder_id
+def list_files_in_company(service, company_folder_id):
+    query = f"'{company_folder_id}' in parents"
+    
+    result = service.files().list(
+        q=query,
+        fields="files(id, name, mimeType)"
+    ).execute()
+    
+    return result["files"]
 
 if __name__ == "__main__":
+    #Construye el servicio
     service = build_drive_service()
-    root_folder_id = os.getenv("DRIVE_ID")
-    folder_list = list_company_folders(service, root_folder_id)
+    #Consigue el ID de la carpeta de Drive dentro de .env
+    root_folder_id = DRIVE_ROOT_FOLDER_ID
+    #Imprime el servicio
     print(f"Conexion exitosa: {service}")
-    print(f"Carpetas encontradas: {folder_list}")
+
+    #Lista las carpetas de cada compañia en el Drive
+    companies = list_company_folders(service, root_folder_id)
+    print(f"Empresas: {companies}")
+
+    #Consigue la primera compañia 
+    first_company = companies[0]
+    #Busca en la primera compañia conseguida y trae sus archivos
+    files = list_files_in_company(service, first_company["id"])
+    #imprime los archivos dentro de la compañia (carpeta)
+    print(f"Archivos en {first_company["name"]}: {files}")
